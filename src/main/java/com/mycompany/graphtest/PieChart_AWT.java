c60ba463f95dabcdd6cf2865557df08c9820c52d
Gaurav <gauravpaudel2013@gmail.com>
2021-01-06 16:50:28 +0545

    Initial Commit

---
+++ src/main/java/com/mycompany/graphtest/PieChart_AWT.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.graphtest;
+
+/**
+ *
+ * @author Dell
+ */
+class PieChart_AWT {
+
+    PieChart_AWT(String mobile_Sales) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setSize(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
